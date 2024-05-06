e4fae525e79dc7596d2ebb10cc9fa8bb70548d1e
azurecoders <azfarchannel54321@gmail.com>
2024-05-07 00:44:54 +0500

    Initial Commit

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -5,19 +5,34 @@ const Sidebar = () => {
   return (
     <section className="sidebar">
       <div className="menu">
-        <Link to="/" name="menu">
+        <Link to="/" name="menu" rel="noopener noreferrer" aria-label="menu">
           <HiOutlineMenuAlt2 size={"1.5rem"} />
         </Link>
-        <Link to="/" name="home">
+        <Link to="/" name="home" rel="noopener noreferrer" aria-label="home">
           <HiOutlineHome size={"1.5rem"} />
         </Link>
-        <Link to="/cart" name="cart">
+        <Link
+          to="/cart"
+          name="cart"
+          rel="noopener noreferrer"
+          aria-label="cart"
+        >
           <CiShoppingCart size={"1.5rem"} />
         </Link>
-        <Link to="/fav" name="favourites">
+        <Link
+          to="/fav"
+          name="favourites"
+          rel="noopener noreferrer"
+          aria-label="favourites"
+        >
           <CiHeart size={"1.5rem"} />
         </Link>
-        <Link to="/orders" name="orders">
+        <Link
+          to="/orders"
+          name="orders"
+          rel="noopener noreferrer"
+          aria-label="orders"
+        >
           <CiDeliveryTruck size={"1.5rem"} />
         </Link>
       </div>
